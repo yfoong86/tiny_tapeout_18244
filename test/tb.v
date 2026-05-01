80e4585b6850a0345f22c36546f89fdebddd7b6e
Yansi Foong <yansifoong@yansis-mbp.wifi.local.cmu.edu>
2026-05-01 15:20:13 -0400

    seventh attempt

--- test/tb.v
+++ test/tb.v
@@ -23,8 +23,8 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 `ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
+  supply1 VPWR = 1'b1;
+  supple0 VGND = 1'b0;
 `endif
 
   // Replace tt_um_example with your module name:
